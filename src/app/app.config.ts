113127f38f0ab43a7758883f591fba930657178a
Temo <temuri.pkhaladze@space.ge>
2026-06-10 15:23:51 +0400

    feat: update service worker registration strategy and add service worker registration script

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000',
+      registrationStrategy: 'registerImmediately',
     }),
   ],
 };
